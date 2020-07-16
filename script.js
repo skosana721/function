92eca752ab875ea104b04219ea15ae89ae986521
Masekolg <masekolg@gmail.com>
2020-07-16 14:21:41 +0200

    array function

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var num = window.prompt();
-var str = num.toString();
-var result = [str[0]];
+let nums = [24,8,23,32,5,62];
 
-for (var x = 2; x < str.length; x++) {
-  if (str[x - 1] % 1 === 0 && str[x] % 1 === 0) {
-    result.push('-', str[x]);
-  } else {
-    result.push(str[x]);
-  }
+function arrange(a, b) {
+  if (a > b) return 1;
+  if (b > a) return -1;
+
+  return 0;
 }
-console.log(result.join(''));
+
+nums.sort(arrange);
